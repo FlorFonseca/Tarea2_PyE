9515afe05ff160bbb0f0246e9a7040c8d669fd76
FlorFonseca <florencia.fonseca@correo.ucu.edu.uy>
2025-05-30 13:08:10 -0300

    arreglando los valores de las constantes y sacando la densidad estimada de la parte 4 y 5

--- parte2.1y2.py
+++ parte2.1y2.py
@@ -8,6 +8,7 @@ import seaborn as sns
 
 def algoritmoPseudoaleatorio(a, c ,m, cantSimulaciones):
     x = 1550
+    #El valor de x corrsponde al horario de la comuptadora
     semilla : int = int(x)
     if gcd(a, m) == 1 and semilla > 0:
         resultados = []
@@ -22,16 +23,16 @@ def algoritmoPseudoaleatorio(a, c ,m, cantSimulaciones):
         print("Error: parámetros inválidos")
         return []
     
-muestra = algoritmoPseudoaleatorio(117, 223, 200, 100)
+muestra = algoritmoPseudoaleatorio(1664525, 1013904223,4294967296, 100)
 print(muestra)
 
 #Usamos Matplot y Seaborn para hacer el histograma de los datos, junto con su curva de densidad.
 plt.hist(muestra, bins=15, color='blue', edgecolor='black', density=True)
-#Density= true normaliza el gráfico, esto es porque seaborn hace la curva de densidad de acurdo a la gráfica normalizada
+#Density= true normaliza el histograma
 sns.kdeplot(muestra, bw_adjust=0.5, color='red', label='Densidad estimada', fill=True)
 plt.xticks(np.linspace(0,1, 21))
 plt.xlabel('Valor')
-plt.ylabel('Frecuencia')
+plt.ylabel('Densidad')
 plt.title('Histograma')
 plt.legend()
 plt.show()
--- parte2.4y5.py
+++ parte2.4y5.py
@@ -1,6 +1,5 @@
 import math
 import matplotlib.pyplot as plt
-import seaborn as sns
 import numpy as np
 from math import gcd
 
@@ -30,12 +29,11 @@ def transformar_a_cauchy(lista_uniformes):
 def densidad_cauchy(x):
     return 1 / (math.pi * (1 + x**2))
 
-uniformes = algoritmoPseudoaleatorio(a=117, c=223, m=200, cantSimulaciones=100)
+uniformes = algoritmoPseudoaleatorio(1664525, 1013904223, 4294967296, 100)
 muestra_cauchy = transformar_a_cauchy(uniformes)
 
 plt.figure(figsize=(10, 6))
 plt.hist(muestra_cauchy, bins=30, density=True, color='orange', edgecolor='black', label='Muestra simulada')
-sns.kdeplot(muestra_cauchy, bw_adjust=0.5, color='red', label='Densidad estimada', fill=True)
 x_vals = np.linspace(-10, 10, 1000)
 y_vals = [densidad_cauchy(x) for x in x_vals]
 plt.plot(x_vals, y_vals, color='blue', label='Densidad teórica Cauchy')
